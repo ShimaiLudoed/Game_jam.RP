de91d2ba28775b8fb7cbfd4711da5e52565760b2
tutugol <s4mararunner@mail.ru>
2024-04-13 20:46:41 +0300

    autosave

--- NEW_game_jam/Assets/Scripts/Enemy.cs
+++ NEW_game_jam/Assets/Scripts/Enemy.cs
@@ -3,30 +3,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
-    public int maxHealth = 150;
-    private int currentHealth;
+    protected bool alive = true;
+    protected int maxHealth;
+    protected int currentHealth;
+    protected float speed;
+    protected int damage;
+    protected float fov;
 
-    private void Start()
+    protected Transform player;
+
+    protected virtual void Start()
     {
-        currentHealth = maxHealth;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    public void TakeDamage(int damage)
+    protected virtual void Update()
     {
-        currentHealth -= damage;
-        if (currentHealth <= 0)
+        if (alive)
         {
-            Die();
+            if (IsPlayerVisible())
+            {
+                Agressive();
+            }
+            else
+            {
+                Chill();
+            }
         }
     }
 
-    void Die()
+    protected bool IsPlayerVisible()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer <= fov)  // Проверяем расстояние до игрока
+        {
+            Vector3 directionToPlayer = (player.position - transform.position).normalized;
+            float angleToPlayer = Vector3.Angle(transform.up, directionToPlayer);
+            // Проверяем угол между направлением взгляда противника и направлением к игроку
+            
+            if (angleToPlayer <= fov / 2)
+            // Проверяем, находится ли игрок в поле зрения противника (угол меньше половины угла зрения)    
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    protected virtual void Chill()
+    {
+        
+    }
+
+    protected virtual void Agressive()
+    {
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
+    }
+
+    protected virtual void Dead()
+    {
+        
+    }
+
+    public abstract void TakeDamage(int damage);
+
+    protected virtual void Die()
     {
         Debug.Log("ENEMY DEAD");
-
-        GetComponent<Collider2D>().enabled = false;
-        this.enabled = false;
+        alive = false;
+        Dead();
     }
 }
--- NEW_game_jam/Assets/Scripts/Melee.cs
+++ NEW_game_jam/Assets/Scripts/Melee.cs
@@ -10,14 +10,11 @@ public class Melee : MonoBehaviour
     public float attackRange = 0.5f; 
  
     public int attackDamage = 40; 
- 
-    // Флаг для отслеживания состояния атаки 
-    private bool isAttacking = false; 
- 
-    // Используйте один метод Update для обработки ввода и вызова атаки 
+    
+    private bool isAttacking = false;
+    
     void Update() 
     { 
-        // Проверяем, если атака уже идет, то выходим из метода 
         if (isAttacking) 
         { 
             return; 
@@ -25,36 +22,28 @@ public class Melee : MonoBehaviour
  
         if (Input.GetMouseButtonDown(0)) 
         { 
-            // Запускаем корутину для атаки 
             StartCoroutine(AttackCoroutine()); 
         } 
     } 
  
     IEnumerator AttackCoroutine() 
     { 
-        // Устанавливаем флаг атаки в true 
         isAttacking = true; 
- 
-        //player animation  
- 
-        //detected enemy  
-        Collider2D[] hitEnemy = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, EnemyLay); 
-        //damage  
+        
+        Collider2D[] hitEnemy = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, EnemyLay);
+        
         foreach (Collider2D enemy in hitEnemy) 
         { 
             Debug.Log("We hit " + enemy.name); 
             enemy.GetComponent<Enemy>().TakeDamage(attackDamage); 
         } 
- 
-        // Ждем некоторое время перед следующей атакой (например, 0.5 секунды) 
-        yield return new WaitForSeconds(0.5f); 
- 
-        // Устанавливаем флаг атаки в false 
+        
+        yield return new WaitForSeconds(0.5f); //кулдаун на атаку 5 мс
+        
         isAttacking = false; 
     } 
- 
-    // Метод для визуализации радиуса атаки в редакторе 
-    private void OnDrawGizmosSelected() 
+    
+    private void OnDrawGizmosSelected() // Метод для визуализации радиуса атаки в редакторе
     { 
         if (attackPoint == null) 
         { 
