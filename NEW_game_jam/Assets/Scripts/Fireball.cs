8365d986c6de20d719179a0199326a2f34b5c676
tutugol <s4mararunner@mail.ru>
2024-04-13 11:46:12 +0300

    autosave

--- NEW_game_jam/Assets/Scripts/Fireball.cs
+++ NEW_game_jam/Assets/Scripts/Fireball.cs
@@ -1,27 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
-    public Transform firePoint;
-    public GameObject fireballPrefab;
-
-    public float bulletForce = 20f;
-    public bool hit = false;
-    
-    void Update()
+    public GameObject HitEffect;
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Shoot();
-        }
-    }
-
-    void Shoot()
-    {
-        GameObject bullet = Instantiate(fireballPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        GameObject effect = Instantiate(HitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 1f);
+        Destroy(gameObject);
     }
 }
